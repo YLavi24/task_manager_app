1aff1c9d4d7a04a242e5106acee2b3589d882528
YLavi24 <Yehudah.lavi1@gmail.com>
2025-07-17 00:26:17 +0300

    fixing cors issue

--- client/src/config.js
+++ client/src/config.js
@@ -2,4 +2,8 @@ const config = {
   API_BASE_URL: process.env.REACT_APP_API_URL || 'https://taskmanagerapp-production.up.railway.app'
 };
 
+// Debug log to see what URL is being used
+console.log('API_BASE_URL:', config.API_BASE_URL);
+console.log('REACT_APP_API_URL env var:', process.env.REACT_APP_API_URL);
+
 export default config;
